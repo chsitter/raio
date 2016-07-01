0d6869ede9a9ca4b755d96a95ef9c401946662e8
Christoph Sitter <christoph.sitter@caplin.com>
2016-07-01 09:35:12 +0100

    timers and exeute works, need to block until thread is ready and implement Future

--- src/lib.rs
+++ src/lib.rs
@@ -26,14 +26,16 @@ pub struct Future;
 
 pub trait Executor : Drop {
     fn new(name: &str) -> Self;
-    fn execute<T, F: Fn(T) + Send + 'static>(&self, callback: F, context: T);
-    fn schedule<T, F: Fn(T) + Send + 'static>(&self, callback: F, context: T, delay: Duration) -> Future;
+    fn execute<F: Fn() + Send + 'static>(&self, callback: F);
+    fn schedule<F: Fn() + Send + 'static>(&self, callback: F, delay: Duration) -> Future;
 
     fn shutdown(&self);
     fn join(&mut self);
 
     fn accept<F: Fn(&mut TcpListener) -> EventControl + Send>(&self, listener: TcpListener, callback: F);
     fn read<F: Fn(&mut TcpStream) -> EventControl + Send>(&self, stream: TcpStream, callback: F);
+
+    fn notify(&self);
 }
 
 impl AsyncTcpListener for TcpListener {
@@ -54,6 +56,13 @@ impl AsyncTcpStream for TcpStream {
 
 enum ThreadMessage {
     Shutdown,
+    Execute {
+        callback: Box<Fn() + Send>
+    },
+    Schedule {
+        delay: Duration,
+        callback: Box<Fn() + Send>
+    },
     AddAcceptEvent {
         fd: i32,
         callback: Box<Fn(&mut TcpListener) -> EventControl + Send>
@@ -66,27 +75,50 @@ enum ThreadMessage {
 
 pub struct SingleThreadedExecutor {
     join_handle: Mutex<Option<JoinHandle<()>>>,  //TODO: make RefCell to not need to have mut executors?
+    kqid: i32,
     sender: Mutex<Sender<ThreadMessage>>
 }
 
 impl Executor for SingleThreadedExecutor {
     fn new(name: &str) -> Self {
 
-        let (tx, rx): (Sender<ThreadMessage>, Receiver<ThreadMessage>)= channel();
-        SingleThreadedExecutor {
-            sender: Mutex::new(tx),
-            join_handle: Mutex::new(Some(thread::Builder::new().name(name.to_string()).spawn( move || {
-                executor_loop(rx);
-            }).unwrap()))
+        let (tx, rx): (Sender<ThreadMessage>, Receiver<ThreadMessage>) = channel();
+        let kq;
+        unsafe {
+             kq = unsafe { libc::kqueue() as i32 };
         }
+
+        let x = SingleThreadedExecutor {
+            sender: Mutex::new(tx),
+            kqid: kq,
+            join_handle: Mutex::new(Some(thread::Builder::new().name(name.to_string()).spawn( move || {
+                executor_loop(kq, rx);
+            }).unwrap()))
+        };
+
+        thread::sleep(Duration::new(2, 0)); //TODO: use a condvar to wait for everything to be set up
+        x
     }
 
-    fn execute<T, F: Fn(T) + Send + 'static>(&self, callback: F, context: T) {
-        unimplemented!()
+    fn execute<F: Fn() + Send + 'static>(&self, callback: F) {
+        let s = self.sender.lock().unwrap();
+        s.send(ThreadMessage::Execute {
+            callback: Box::new(callback)
+        }).unwrap();
+
+        self.notify();
     }
 
-    fn schedule<T, F: Fn(T) + Send + 'static>(&self, callback: F, context: T, delay: Duration) -> Future {
-        unimplemented!()
+    fn schedule<F: Fn() + Send + 'static>(&self, callback: F, delay: Duration) -> Future {
+        let s = self.sender.lock().unwrap();
+        s.send(ThreadMessage::Schedule {
+            delay: delay,
+            callback: Box::new(callback)
+        }).unwrap();
+
+        self.notify();
+
+        Future
     }
 
     fn accept<F: Fn(&mut TcpListener) -> EventControl + Send + 'static>(&self, listener: TcpListener, callback: F) {
@@ -95,6 +127,8 @@ impl Executor for SingleThreadedExecutor {
             fd: listener.into_raw_fd(),
             callback: Box::new(callback)
         }).unwrap();
+        
+        self.notify();
     }
 
     fn read<F: Fn(&mut TcpStream) -> EventControl + Send + 'static>(&self, stream: TcpStream, callback: F) {
@@ -103,6 +137,8 @@ impl Executor for SingleThreadedExecutor {
             fd: stream.into_raw_fd(),
             callback: Box::new(callback)
         }).unwrap();
+
+        self.notify();
     }
 
     fn shutdown(&self) {
@@ -110,6 +146,23 @@ impl Executor for SingleThreadedExecutor {
         match s.send(ThreadMessage::Shutdown) {
             Ok(()) => {},
             Err(e) => println!("Error occurred!! {}", e)
+        }
+        
+        self.notify();
+    }
+
+    fn notify(&self) {
+        let ev = libc::kevent {
+            ident: 0,
+            filter: libc::EVFILT_USER,
+            flags: 0,
+            fflags: libc::NOTE_TRIGGER,
+            data: 0,
+            udata: std::ptr::null_mut()
+        };
+
+        unsafe {
+            libc::kevent(self.kqid, &ev, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
         }
     }
 
@@ -132,7 +185,7 @@ enum CallbackType {
     READ(Box<Fn(&mut TcpStream) -> EventControl>)
 }
 
-fn executor_loop(receiver: Receiver<ThreadMessage>) {
+fn executor_loop(kq: i32, receiver: Receiver<ThreadMessage>) {
     let mut nev;
 
     let num_fds: usize = unsafe {
@@ -141,57 +194,96 @@ fn executor_loop(receiver: Receiver<ThreadMessage>) {
                 rlim.rlim_cur   //TODO: should we be using rlim_max?
     } as usize;
 
-    let mut ev_list: [libc::kevent; 32] = [ libc::kevent { ident: 0, filter: 0, flags: 0, fflags: 0, data: 0, udata: std::ptr::null_mut() };32];
-    let timeout = Box::into_raw(Box::new(libc::timespec { tv_sec: 0, tv_nsec: 100 })); //Don't want to timeout, should use a pipe to notify that thread about new events
-    let kq = unsafe { libc::kqueue() as i32 };
-
     let mut readevents: Vec<Option<(CallbackType)>> = Vec::with_capacity(num_fds);  //TODO: This should probably be in local storage
     for i in 0..num_fds {
         readevents.push(None);
     }
 
+    let mut timer: Option<Box<Fn()>> = None;
+
+    let mut ev_list: [libc::kevent; 32] = [ libc::kevent { ident: 0, filter: 0, flags: 0, fflags: 0, data: 0, udata: std::ptr::null_mut() };32];
+
+
+    let user_ev = libc::kevent {
+        ident: 0,
+        filter: libc::EVFILT_USER,
+        flags: libc::EV_ADD | libc::EV_CLEAR,
+        fflags: 0,
+        data: 0,
+        udata: std::ptr::null_mut()
+    };
+
+    unsafe {
+        libc::kevent(kq, &user_ev, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+    }
+
+
+
     loop {
-        match receiver.try_recv() { //This should be registered with  kevent too
-            Ok(ThreadMessage::Shutdown)        => break,
-            Ok(ThreadMessage::AddAcceptEvent{ fd, callback }) => {
-                println!("registering accept event for fd {}", fd);
-                readevents[fd as usize] = Some((CallbackType::ACCEPT(callback))); //TOOD: check whether it's better this way or to move it on to the context of the kevent
+        loop {
+            match receiver.try_recv() { //This should be registered with  kevent too
+                Ok(ThreadMessage::Shutdown)        => break,
+                Ok(ThreadMessage::AddAcceptEvent{ fd, callback }) => {
+                    println!("registering accept event for fd {}", fd);
+                    readevents[fd as usize] = Some((CallbackType::ACCEPT(callback))); //TOOD: check whether it's better this way or to move it on to the context of the kevent
 
-                let ev_set = libc::kevent {
-                    ident: fd as libc::uintptr_t,
-                    filter: libc::EVFILT_READ,
-                    flags: libc::EV_ADD,
-                    fflags: 0,
-                    data: 0,
-                    udata: std::ptr::null_mut()
-                };
+                    let ev_set = libc::kevent {
+                        ident: fd as libc::uintptr_t,
+                        filter: libc::EVFILT_READ,
+                        flags: libc::EV_ADD,
+                        fflags: 0,
+                        data: 0,
+                        udata: std::ptr::null_mut()
+                    };
 
-                unsafe {
-                    libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                    unsafe {
+                        libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                    }
+                },
+                Ok(ThreadMessage::AddReadEvent{ fd, callback }) => {
+                    println!("added read event");
+                    readevents[fd as usize] = Some((CallbackType::READ(callback)));
+
+                    let ev_set = libc::kevent {
+                        ident: fd as libc::uintptr_t,
+                        filter: libc::EVFILT_READ,
+                        flags: libc::EV_ADD,
+                        fflags: 0,
+                        data: 0,
+                        udata: std::ptr::null_mut()
+                    };
+
+                    unsafe {
+                        libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                    }
+                },
+                Ok(ThreadMessage::Execute{ callback }) => {
+                    callback();
+                },
+                Ok(ThreadMessage::Schedule{ delay, callback }) => {
+                    let ev_set = libc::kevent {
+                        ident: 0,
+                        filter: libc::EVFILT_TIMER,
+                        flags: libc::EV_ADD | libc::EV_ENABLE,
+                        fflags: 0,
+                        data: (delay.as_secs() * 1000 + (delay.subsec_nanos() / 1000000u32) as u64) as isize,
+                        udata: std::ptr::null_mut()
+                    };
+
+                    timer = Some(callback);
+
+                    unsafe {
+                        libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                    }
+                },
+                Err(_) => {
+                    break;
                 }
             }
-            Ok(ThreadMessage::AddReadEvent{ fd, callback }) => {
-                println!("added read event");
-                readevents[fd as usize] = Some((CallbackType::READ(callback)));
-
-                let ev_set = libc::kevent {
-                    ident: fd as libc::uintptr_t,
-                    filter: libc::EVFILT_READ,
-                    flags: libc::EV_ADD,
-                    fflags: 0,
-                    data: 0,
-                    udata: std::ptr::null_mut()
-                };
-
-                unsafe {
-                    libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
-                }
-            }
-            Err(_) => {}
         }
 
         unsafe {
-            nev = libc::kevent(kq, std::ptr::null(), 0, ev_list.as_mut_ptr(), 32, timeout );
+            nev = libc::kevent(kq, std::ptr::null(), 0, ev_list.as_mut_ptr(), 32, std::ptr::null_mut());
         }
 
         match nev {
@@ -200,65 +292,78 @@ fn executor_loop(receiver: Receiver<ThreadMessage>) {
             num => {
                 //println!("got {} events", num);
 
-                for _ in  0..num {
+                for i in  0..num {
                     let fd = ev_list[0].ident as i32;
-                    if readevents[fd as usize].is_some() {
-                        let mut deleted = false;
-                        if let Some(ref cb_type) = readevents[fd as usize] {
-                            match cb_type {
-                                &CallbackType::ACCEPT(ref cb)  => unsafe {
-                                    println!("accepting from fd {}", fd);
-                                    let mut listener = TcpListener::from_raw_fd(fd);
-                                    match (cb)(&mut listener) {
-                                        EventControl::DELETE => {
-                                            let ev_set = libc::kevent {
-                                                ident: fd as libc::uintptr_t,
-                                                filter: libc::EVFILT_READ,
-                                                flags: libc::EV_DELETE,
-                                                fflags: 0,
-                                                data: 0,
-                                                udata: std::ptr::null_mut()
-                                            };
+                    let filt = ev_list[0].filter as i16;
+                    match filt {
+                        libc::EVFILT_READ => {
+                            if readevents[fd as usize].is_some() {
+                                let mut deleted = false;
+                                if let Some(ref cb_type) = readevents[fd as usize] {
+                                    match cb_type {
+                                        &CallbackType::ACCEPT(ref cb)  => unsafe {
+                                            println!("accepting from fd {}", fd);
+                                            let mut listener = TcpListener::from_raw_fd(fd);
+                                            match (cb)(&mut listener) {
+                                                EventControl::DELETE => {
+                                                    let ev_set = libc::kevent {
+                                                        ident: fd as libc::uintptr_t,
+                                                        filter: libc::EVFILT_READ,
+                                                        flags: libc::EV_DELETE,
+                                                        fflags: 0,
+                                                        data: 0,
+                                                        udata: std::ptr::null_mut()
+                                                    };
 
-                                            libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
-                                            deleted = true;
+                                                    libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                                                    deleted = true;
+                                                },
+                                                EventControl::KEEP => {
+                                                    listener.into_raw_fd();
+                                                }
+                                            }
                                         },
-                                        EventControl::KEEP => {
-                                            listener.into_raw_fd();
-                                        }
-                                    }
-                                },
-                                &CallbackType::READ(ref cb) => unsafe {
-                                    println!("reading from fd {}", fd);
-                                    let mut stream = TcpStream::from_raw_fd(fd);
-                                    match (cb)(&mut stream) {
-                                        EventControl::DELETE => {
-                                            let ev_set = libc::kevent {
-                                                ident: fd as libc::uintptr_t,
-                                                filter: libc::EVFILT_READ,
-                                                flags: libc::EV_DELETE,
-                                                fflags: 0,
-                                                data: 0,
-                                                udata: std::ptr::null_mut()
-                                            };
+                                        &CallbackType::READ(ref cb) => unsafe {
+                                            println!("reading from fd {}", fd);
+                                            let mut stream = TcpStream::from_raw_fd(fd);
+                                            match (cb)(&mut stream) {
+                                                EventControl::DELETE => {
+                                                    let ev_set = libc::kevent {
+                                                        ident: fd as libc::uintptr_t,
+                                                        filter: libc::EVFILT_READ,
+                                                        flags: libc::EV_DELETE,
+                                                        fflags: 0,
+                                                        data: 0,
+                                                        udata: std::ptr::null_mut()
+                                                    };
 
-                                            libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
-                                            deleted = true;
-                                        },
-                                        EventControl::KEEP  => {
-                                            stream.into_raw_fd();
+                                                    libc::kevent(kq, &ev_set, 1, std::ptr::null_mut(), 0, std::ptr::null_mut());
+                                                    deleted = true;
+                                                },
+                                                EventControl::KEEP  => {
+                                                    stream.into_raw_fd();
+                                                }
+                                            }
                                         }
                                     }
                                 }
+                                if deleted == true {
+                                    readevents[fd as usize].take();
+                                }
+                            }
+                        },
+                        libc::EVFILT_TIMER => {
+                            if let Some(ref c) = timer {
+                                c();
                             }
                         }
-                        if deleted == true {
-                            readevents[fd as usize].take();
+                        libc::EVFILT_USER => {
                         }
-                        //println!("event for fd {}", fd);
+                        x => {
+                            println!("unhandled event {}", x);
+                        }
                     }
                 }
-                //println!("finished {} events", num);
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate raio;
 
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::{TcpListener};
 use raio::{Executor, SingleThreadedExecutor, AsyncTcpListener, AsyncTcpStream, EventControl};
 
 fn main() {
-    let mut executor = SingleThreadedExecutor::new("foo");
+    let mut executor = SingleThreadedExecutor::new("executor-0");
     let listener = TcpListener::bind("127.0.0.1:5432").unwrap();
 
     listener.accept_async( &executor, | list | {
@@ -35,6 +37,23 @@ fn main() {
         println!("accepted {:?}", addr);
         EventControl::KEEP
     });
+
+
+    executor.execute(|| {
+        println!("hi0 on thread {:?}", thread::current());
+    });
+
+    executor.execute(|| {
+        println!("hi1 on thread {:?}", thread::current());
+    });
+    
+    executor.execute(|| {
+        println!("hi2 on thread {:?}", thread::current());
+    });
+
+    executor.schedule( || {
+        println!("timer  thread {:?}", thread::current());
+    }, Duration::new(2, 0));
 
     executor.join();
 }
